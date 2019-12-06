6422f5acd12d205a8ec744b7396a7bf42a3c410c
Srikumar K. S <sriku@imaginea.com>
2019-12-06 16:59:03 +0530

    Mentioned that script must be added to head

--- services/bulma/index.js
+++ services/bulma/index.js
@@ -10,7 +10,8 @@ I.boot = function (name, resid, query, headers, config) {
     I.dom('bulma/fonts', {
         op: 'set',
         tag: 'script',
-        attrs: { defer: true, src: 'https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css'}
+        attrs: { defer: true, src: 'https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css'},
+        childOf: 'head'
     });
 
     I.boot = null;
